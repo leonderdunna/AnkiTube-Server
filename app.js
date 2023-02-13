77ad9b0de85ef67e063a0adf605a2f61b81861d7
Leonard Menzel <leonderdunna@gmail.com>
2023-02-13 16:54:47 +0100

    added deck route

--- app.js
+++ app.js
@@ -25,12 +25,20 @@ const cardSchema = new mongoose.Schema({
 });
 const Card = mongoose.model("Card", cardSchema)
 
+const deckSchema = new mongoose.Schema({
+    name: String,
+    uuid: String,
+    ids: [],
+    children: []
+})
+const Deck = mongoose.model("Deck",deckSchema)
+
 // CREATE Routes
 let app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/card/', (req, res) => {
     Card.find({}, (err, found) => {
         if (!err) {
             res.send(found);
@@ -40,7 +48,7 @@ app.get('/', (req, res) => {
         }
     })
 });
-app.get('/:deck', (req, res) => {
+app.get('/card/:deck', (req, res) => {
     Card.find({deck_id: req.params.deck}, (err, found) => {
         if (!err) {
             res.send(found);
@@ -51,32 +59,91 @@ app.get('/:deck', (req, res) => {
     })
 });
 
-app.post("/", async (req, res) => {
+
+
+
+
+
+app.get('/deck/', (req, res) => {
+    Deck.find({}, (err, found) => {
+        if (!err) {
+            res.send(found);
+        } else {
+            console.log(err);
+            res.send("Some error occured!")
+        }
+    })
+});
+app.get('/deck/:uuid', (req, res) => {
+    Deck.find({uuid: req.params.uuid}, (err, found) => {
+        if (!err) {
+            res.send(found);
+        } else {
+            console.log(err);
+            res.send("Some error occured!")
+        }
+    })
+});
+
+
+
+
+
+
+app.post("/card/", async (req, res) => {
     let validate = false
     if (req.body.id != null && req.body.deck_id != null) validate = true
     if (validate) {
         await Card.find({id: req.body.id}).remove()
         new Card(req.body).save().then(r => {
-            res.send("Successfully added")
+            res.send({message:"Successfully added"})
         })
     } else {
         console.log(req.body)
         res.status(400).send({message: "die karte kann nicht gespeichert werden, weil sie den anforderungen nicht entspricht"})
     }
 })
-app.delete("/:id", (req, res) => {
+app.delete("/card/:id", (req, res) => {
     Card.find({id: req.params.id}, (err, result) => {
         console.log("Deleting:", result)
 
     }).remove()
-    res.send("Karte wird gelösht, wenn sie existiert")
+    res.send({message: "Karte wird gelösht, wenn sie existiert"})
 })
 
-// START SERVER
+
+app.post("/deck", async (req, res) => {
+    let validate = false
+    if (req.body.uuid != null && req.body.name != null && req.body.children != null && req.body.ids != null) validate = true
+    if (validate) {
+        await Deck.find({uuid: req.body.uuid}).remove()
+        new Deck(req.body).save().then(r => {
+            res.send({message: "Successfully added"})
+        })
+    } else {
+        console.log(req.body)
+        res.status(400).send({message: "dieses Deck kann nicht gespeichert werden, weil es den anforderungen nicht entspricht"})
+    }
+})
+app.delete("/deck/:uuid", (req, res) => {
+    Deck.find({uuid: req.params.uuid}, (err, result) => {
+        console.log("Deleting:", result, req.params.uuid)
+
+    }).remove()
+    res.send({message:"Deck wird gelösht, wenn es existiert"})
+})
+
+// START SERVER testing
+app.listen(3000, () => {
+    console.log("AnkiTube Server Testing is listening on port 3000")
+})
+
+
+// START SERVER Prod...
+/*
 let server = https.createServer({
     key: fs.readFileSync("/etc/letsencrypt/live/139-162-135-50.ip.linodeusercontent.com/privkey.pem","utf8"),
     cert: fs.readFileSync("/etc/letsencrypt/live/139-162-135-50.ip.linodeusercontent.com/fullchain.pem", "utf8")
 },app);
-
 server.listen(80);
-
+*/
